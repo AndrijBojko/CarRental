861beece10aa85ca6c667fba05a218c23023adff
AndrijBojko <andrij.boijko@gmail.com>
2017-05-19 17:47:46 +0300

    Update Drop.sql

--- CarRental/CarRental.Database/Drop.sql
+++ CarRental/CarRental.Database/Drop.sql
@@ -1,10 +1,10 @@
 USE master;
 GO
 
+DROP TABLE [CarRental].dbo.[Order];
 DROP TABLE [CarRental].dbo.[Manager];
 DROP TABLE [CarRental].dbo.[Car];
 DROP TABLE [CarRental].dbo.[Customer];
-DROP TABLE [CarRental].dbo.[Order];
 
 
 ALTER DATABASE [CarRental] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
